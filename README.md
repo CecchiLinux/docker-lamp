115e6866cd74c682660c97f0f4bdf1dbef7ffbd1
CecchiLinux <enrico.ceccolini@mail.com>
2017-10-12 21:30:42 +0000

    minor fix

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # docker-lamp
+
+cd docker-lamp
+docker-compose up -d
+
+visit localhost:80
+
+phpmyadmin login: 
+* localhost:8081
+* username: root
+* password: password
--- html/index.php
+++ html/index.php
@@ -1,4 +1,5 @@
 <?php
 mysqli_connect("mysql", "root", "psw") or die(mysqli_error());
-echo "Sito 2. Connected to MySQL<br />";
+echo "Connected to MySQL<br />";
+phpinfo();
 ?>
